73a22c0c57885b8a452dfcb89333243a2546b32f
Sandesh Ghimire <sandeshghimire202@gmail.com>
2024-01-02 20:55:16 +0545

    variables

---
+++ day1/variable.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Variables are containers used to store value in the program. 
+//Declaring Variable
+String name="Sandesh";
+bool isMale=true;
+int age=21;
+double height=5.8;
+num mobile=9846930428;
+
+//printing values:
+
+print(name);
+print(isMale);
+print(age);
+print(height);
+print(mobile);
+
+
+}
